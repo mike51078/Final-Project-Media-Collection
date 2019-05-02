a5535ca25ce9d5b636cb6e301dba06697377bfae
elcid00 <elcid00@aol.com>
2019-05-02 12:06:59 -0400

    thurs morn updates

---
+++ client/src/components/Footer/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+function Footer() {
+	return (
+		<footer className="footer">
+			<div className="bottom">
+				<h3>Footer</h3>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
