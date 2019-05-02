97a89846b52db1041548085cc3d7a111856b16a9
elcid00 <elcid00@aol.com>
2019-05-02 14:27:21 -0400

    fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
+
+import Navbar from './components/layout/Navbar';
+import Home from './components/Pages/Home';
+import User from './components/Pages/User';
+import Login from './components/auth/login';
+
+import './App.css';
+
+function onAuthRequired({ history }) {
+	history.push('/login');
+}
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<div className="App-header">
-					<h2>Final Project</h2>
-				</div>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
-			</div>
+			<Router>
+				<Security
+					issuer="https://dev-711148.okta.com/oauth2/default"
+					client_id="0oaixp18gjbAOIwSD356"
+					redirect_uri={window.location.origin + '/implicit/callback'}
+					onAuthRequired={onAuthRequired}
+				>
+					<div className="App">
+						<Navbar />.
+						<Route path="/" exact={true} component={Home} />
+						<SecureRoute path="/User" exact={true} component={User} />
+						<Route path="/login" render={() => <Login baseUrl="https://dev-711148.okta.com" />} />
+						<Route path="/implicit/callback" component={ImplicitCallback} />
+					</div>
+				</Security>
+			</Router>
 		);
 	}
 }
