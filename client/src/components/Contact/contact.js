d3e7bd80b526cac60046095752b12e06ea68aa8e
elcid00 <elcid00@aol.com>
2019-05-06 14:39:50 -0400

    profile set up

--- client/src/components/Contact/contact.js
+++ client/src/components/Contact/contact.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class Contact extends Component {
+export default class Contact extends Component {
 	render() {
 		return (
 			<div>
@@ -9,5 +9,3 @@ class Contact extends Component {
 		);
 	}
 }
-
-export default Contact;
--- client/src/components/Pages/Home.js
+++ client/src/components/Pages/Home.js
@@ -3,62 +3,59 @@ import { Link } from 'react-router-dom';
 import { withAuth } from '@okta/okta-react';
 
 export default withAuth(
-  class Home extends Component {
-    state = { authenticated: null };
+	class Home extends Component {
+		state = { authenticated: null };
 
-    checkAuthentication = async () => {
-      const authenticated = await this.props.auth.isAuthenticated();
-      if (authenticated !== this.state.authenticated) {
-        this.setState({ authenticated });
-      }
-    };
+		checkAuthentication = async () => {
+			const authenticated = await this.props.auth.isAuthenticated();
+			if (authenticated !== this.state.authenticated) {
+				this.setState({ authenticated });
+			}
+		};
 
-    async componentDidMount() {
-      this.checkAuthentication();
-    }
+		async componentDidMount() {
+			this.checkAuthentication();
+		}
 
-    async componentDidUpdate() {
-      this.checkAuthentication();
-    }
+		async componentDidUpdate() {
+			this.checkAuthentication();
+		}
 
-    login = async () => {
-      this.props.auth.login('/');
-    };
+		login = async () => {
+			this.props.auth.login('/');
+		};
 
-    logout = async () => {
-      this.props.auth.logout('/');
-    };
+		logout = async () => {
+			this.props.auth.logout('/');
+		};
 
-    render() {
-      if (this.state.authenticated === null) return null;
+		render() {
+			if (this.state.authenticated === null) return null;
 
-      const mainContent = this.state.authenticated ? (
-        <div>
-          <p className="lead">
-            You have entered the Do I have It User portal,{' '}
-            <Link to="/User">click here</Link>
-          </p>
-          <button className="btn btn-light btn-lg" onClick={this.logout}>
-            Logout
-          </button>
-        </div>
-      ) : (
-        <div>
-          <p className="lead">
-            If you are a new member email support to get started
-          </p>
-          <button className="btn btn-dark btn-lg" onClick={this.login}>
-            Login
-          </button>
-        </div>
-      );
+			const mainContent = this.state.authenticated ? (
+				<div>
+					<p className="lead">
+						You have entered the Do I have It User portal, <Link to="/User">click here</Link>
+					</p>
+					<button className="btn btn-light btn-lg" onClick={this.logout}>
+						Logout
+					</button>
+				</div>
+			) : (
+				<div>
+					<p className="lead">If you are a new member email support to get started</p>
+					<button className="btn btn-dark btn-lg" onClick={this.login}>
+						Login
+					</button>
+				</div>
+			);
 
-      return (
-        <div className="jumbotron">
-          <h1 className="display-4">Do I have It User Portal</h1>
-          {mainContent}
-        </div>
-      );
-    }
-  }
+			return (
+				<div className="jumbotron">
+					<h1 className="display-4">Do I have It User Portal</h1>
+					{mainContent}
+				</div>
+			);
+		}
+	}
 );
