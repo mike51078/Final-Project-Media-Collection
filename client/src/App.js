25f40f2613a60f7e267aa03f590b3ae2efa0c7db
elcid00 <elcid00@aol.com>
2019-05-06 10:38:34 -0400

    comments in app

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import User from './components/Pages/User';
 import Login from './components/auth/login';
 import './App.css';
 import Profile from './components/Pages/profile';
-
+//Some comments here
 function onAuthRequired({ history }) {
 	history.push('/login');
 }
